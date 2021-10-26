b9b7edec1772b8e55db07df684996a3947adc570
Mark M <otteromkram@gmail.com>
2021-10-26 11:03:53 -0500

    redoing frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,22 @@
-/* eslint-disable */
-import { StrictMode } from "react"
-import { render } from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
-// Main app
-import App from "./components/App"
+import App from "./App";
+import Root from "./Root";
+import * as serviceWorker from "./serviceWorker";
 
-// Main css
-import "bootstrap/dist/css/bootstrap.min.css"
+import "./styles/index.css";
 
-// eslint-disable-next-line
-render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
+ReactDOM.render(
+    <React.StrictMode>
+        <Root>
+            <App />
+        </Root>
+    </React.StrictMode>,
     document.getElementById("root")
-)
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

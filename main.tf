ee9a13636c877dd39a15da76adb5ffcb9e2e32f2
Mark M <otteromkram@gmail.com>
2021-10-31 19:50:30 -0500

    adding data back into the fray

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+    required_providers {
+      docker = {
+        source= "kreuzwerker/docker"
+        version = ">= 2.13.0"
+      }
+    }
+}
+
+provider "docker" {
+  host  = "npipe:////.//pipe//docker_engine"
+}
+resource "docker_image" "nginx" {
+  name  = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name = "email_parser"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
